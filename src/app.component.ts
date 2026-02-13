3b0af834bd72de4028d70ea3c400b101ca08fa3d
weblumea10-cmyk <weblumea10@gmail.com>
2026-02-13 14:34:40 +0500

    1122

---
+++ src/app.component.ts
@@ -0,0 +1,12 @@
+
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [HeaderComponent, FooterComponent],
+})
+export class AppComponent {}
---
+++ src/components/header/header.component.ts
@@ -0,0 +1,9 @@
+
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {}
